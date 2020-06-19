610378cb841c53ef1b8ffef116be810620f46783
Geoffroy <Eliott.geoffroy@gmail.com>
2020-06-19 13:27:29 +0200

    up / down scroll

---
+++ tests/AppTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTests extends TestCase {
+    public function testTestAreWorking() {
+        $this->assertEquals(2, 1+1);
+    }
+}
